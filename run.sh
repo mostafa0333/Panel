dcf872f581aa4e6974750d60d47d6d53acc80792
mostafa0333 <mostafa0333@gmail.com>
2018-01-06 23:31:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,73 @@
-HOMEPATH=home/container	if [[ $@ == *"-fastdl"* ]]; then	  cd $HOMEPATH/csgoecho "FastDL Script by SkyChrome"echo "This will take a few moments. This will automatically setup Fasttdl up for you."echo "Compressing all files."IFS=$'\n'for path in $(find . -type f | egrep '^\./(maps|models|materials|sound)/' | sed 's|^\./||')do        if [ ! -e "$HOMEPATH/fastdl/$path.bz2" ]; then                nice -n +19 ionice -n 2 cp --parents "$path" "$HOMEPATH/fastdl/"                nice -n +19 ionice -n 2 bzip2 -s -1 "$HOMEPATH/fastdl/$path"        fidoneipserver=%ip%ipserver=`echo $ipserver | sed -e 's/\./_/g'`echo "Uploading to a external server."sshpass -p96HCLgHK rsync -a --delete $HOMEPATH/fastdl/ root@p3.quantumpanel.ga:/var/www/html/fastdl/${SERVER_IP}/${SERVER_PORT} echo "Done!"echo "Configuring FastDl up for you."sed -i -e '/sv_downloadurl/d' $HOMEPATH/csgo/cfg/server.cfgsed -i '/^$/N;/^\$/D' %home_path%/csgo/cfg/server.cfgecho >> %home_path%/csgo/cfg/server.cfgecho -n "sv_downloadurl \"http://vps498274.ovh.net/${SERVER_IP}/${SERVER_PORT}\"" >> $HOMEPATH/csgo/cfg/server.cfgecho "Finished FastDl!"echo "Staring..."	else		echo "Skipping FastDL setup, to enable fastdl sync on every startup, type in the commandline -fastdl"	fi
+HOMEPATH=home/container
+
+
+	if [[ $@ == *"-fastdl"* ]]; then
+	
+  cd $HOMEPATH/csgo
+
+echo "FastDL Script by SkyChrome"
+echo "This will take a few moments. This will automatically setup Fasttdl up for you."
+
+echo "Compressing all files."
+
+
+
+IFS=$'\n'
+
+for path in $(find . -type f | egrep '^\./(maps|models|materials|sound)/' | sed 's|^\./||')
+
+do
+
+        if [ ! -e "$HOMEPATH/fastdl/$path.bz2" ]; then
+
+                nice -n +19 ionice -n 2 cp --parents "$path" "$HOMEPATH/fastdl/"
+
+                nice -n +19 ionice -n 2 bzip2 -s -1 "$HOMEPATH/fastdl/$path"
+
+        fi
+
+done
+
+
+
+ipserver=%ip%
+
+ipserver=`echo $ipserver | sed -e 's/\./_/g'`
+
+
+
+echo "Uploading to a external server."
+
+sshpass -p96HCLgHK rsync -a --delete $HOMEPATH/fastdl/ root@p3.quantumpanel.ga:/var/www/html/fastdl/${SERVER_IP}/${SERVER_PORT} 
+
+echo "Done!"
+
+
+
+echo "Configuring FastDl up for you."
+
+sed -i -e '/sv_downloadurl/d' $HOMEPATH/csgo/cfg/server.cfg
+
+sed -i '/^$/N;/^\
+
+$/D' %home_path%/csgo/cfg/server.cfg
+
+echo >> %home_path%/csgo/cfg/server.cfg
+
+echo -n "sv_downloadurl \"http://vps498274.ovh.net/${SERVER_IP}/${SERVER_PORT}\"" >> $HOMEPATH/csgo/cfg/server.cfg
+
+
+
+echo "Finished FastDl!"
+
+echo "Staring..."
+
+./srcds_run -game csgo -console -port ${SERVER_PORT} +ip 0.0.0.0 -strictportbind -norestart $@
+
+	else
+	
+	echo "Skipping FastDL setup, to enable fastdl sync on every startup, type in the commandline -fastdl"
+	
+	./srcds_run -game csgo -console -port ${SERVER_PORT} +ip 0.0.0.0 -strictportbind -norestart $@
+
+	fi
